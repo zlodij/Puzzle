a778f3349ca6d2a8efca9d18f7b7e63358957310
zlodij <ya-zlodij@yandex.ua>
2020-03-05 11:35:29 +0200

    Name of sorter class changed

--- 2020.02.28/src/test/java/ListSorterTester.java
+++ 2020.02.28/src/test/java/ListSorterTester.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class ListSorterTester {
     public static void main(String[] args) {
-        List<Integer> src = new ArrayList<>(Arrays.asList(15, 24, 3, 22, 75, -18, 99, 98, -47, 33, 11, 77, 1, 6, 5));
+        List<Integer> src = new ArrayList<>(Arrays.asList(15, 24, 3, 22, 75, 18, 99, 98, 47, 33, 11, 77, 1, 6, 5));
         System.out.println("source: \t" + src.toString());
 
-        ListSorter sorter = new CallableSorter();
+        ListSorter sorter = new ConcurrentSorter();
         System.out.println("result: \t" + sorter.sort(src).toString());
     }
 }
