b61b4c41558b809a53da6b7fbd14ce816b67d991
zlodij <ya-zlodij@yandex.ua>
2020-03-05 11:18:32 +0200

    Extended with JavaDoc

--- 2020.02.28/src/main/java/puzzle/threads/ListSorter.java
+++ 2020.02.28/src/main/java/puzzle/threads/ListSorter.java
@@ -3,5 +3,10 @@ package puzzle.threads;
 import java.util.List;
 
 public interface ListSorter {
-    List<Integer> sort(List<Integer> in);
+    /**
+     * Sorts a list of integers.
+     * @param source a list of integer values.
+     * @return a sorted list.
+     */
+    List<Integer> sort(List<Integer> source);
 } // end ThreadSorter
